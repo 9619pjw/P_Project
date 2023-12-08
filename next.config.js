4954f2988034404431e8a0ae68d0535eae99e3bc
JokerBear_96 <9619pjw@gmail.com>
2023-12-08 22:52:46 +0900

    fix searchlist

--- next.config.js
+++ next.config.js
@@ -8,13 +8,3 @@ let nextConfig = {
 }
 
 module.exports = nextConfig
-
-// module.exports = nextConfig
-
-// nextConfig = {
-//     reactStrictMode: true,
-//     swcMinify: true,
-//     images: {
-//         domains: ['with-sports-s3.s3.ap-northeast-2.amazonaws.com'],
-//     },
-// }
--- src/app/profile/[id]/profileComponent/ShowSearchList.tsx
+++ src/app/profile/[id]/profileComponent/ShowSearchList.tsx
@@ -32,7 +32,7 @@ export default function ShowSearchList(searchNameResult: SearchNameProps) {
           <td>자기소개</td>
         </tr>
         {searchNameResult.searchNameResult.data.map((user) => (
-          <div key={user.id}>
+          <div key={user.userId}>
             <tr
               style={{
                 display: "flex",
@@ -45,7 +45,7 @@ export default function ShowSearchList(searchNameResult: SearchNameProps) {
               <div
                   style={{ margin: "0 10px" }}
                   onClick={() => {
-                    window.location.href = `/profile/${user.id}`;
+                    window.location.href = `/profile/${user.userId}`;
                   }}
                 >
                 <Image
