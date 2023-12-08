298046341330c4fa9011b167e319ef372c8e9641
JokerBear_96 <9619pjw@gmail.com>
2023-12-08 22:43:46 +0900

    fix next.confilg.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
-
-nextConfig = {
+let nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
         domains: ['with-sports-s3.s3.ap-northeast-2.amazonaws.com'],
     },
 }
+
+module.exports = nextConfig
+
+// module.exports = nextConfig
+
+// nextConfig = {
+//     reactStrictMode: true,
+//     swcMinify: true,
+//     images: {
+//         domains: ['with-sports-s3.s3.ap-northeast-2.amazonaws.com'],
+//     },
+// }
