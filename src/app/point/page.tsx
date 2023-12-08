051d24410c7c3bba423208fa7b81241fe931b6cd
JokerBear_96 <9619pjw@gmail.com>
2023-12-09 03:05:20 +0900

    edit point route

--- src/app/point/page.tsx
+++ src/app/point/page.tsx
@@ -98,7 +98,7 @@ export default function PointPage() {
   const router = useRouter();
 
   const handleGoBack = () => {
-    window.location.href = "/gift";
+    window.location.href = "/gifticon";
   };
 
   return (
