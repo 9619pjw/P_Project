8b869b84a028eed959e17f4618a810efe3c80599
JokerBear_96 <9619pjw@gmail.com>
2023-12-11 02:41:02 +0900

    edit skeleton

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,13 @@
+import { Skeleton } from '@/components/skeleton';
+
 
 export default function Loading() {
 
-    return <>Now Loading..</>
+    return 
+    <>
+        <Skeleton className="h-8 w-full bg-gray-400 my-4" />
+        <Skeleton className="h-8 w-full bg-gray-400 my-4" />
+        <Skeleton className="h-8 w-full bg-gray-400 my-4" />
+        <Skeleton className="h-8 w-full bg-gray-400 my-4" />
+    </>
 }
