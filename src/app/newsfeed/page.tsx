74e91e5dcdf56524d5d92a6e53cf03872be0430c
JokerBear_96 <9619pjw@gmail.com>
2023-12-11 22:34:13 +0900

    fix5

--- src/app/newsfeed/page.tsx
+++ src/app/newsfeed/page.tsx
@@ -98,32 +98,33 @@ function NewsfeedComponent({loadData, fetchNewsfeeds, token} : NewsfeedComponent
                             <p className="text-xs text-gray-500">게시일 : {newsfeed.createdDate}</p>
                         </div>
                     </div>
-                <div className="mb-4 flex justify-center">
-                    <div className="w-128 h-128 overflow-auto">
-                        <img src={newsfeed.feedImgURL} alt="Project Image" className="w-full h-auto rounded-lg" />
-                        <div className="text-center">
-                            <p className="text-gray-900 font-bold">{newsfeed.title}</p>
-                            <p className="text-gray-700">{newsfeed.content}</p>
+                    <div className="mb-4 flex justify-center">
+                        <div className="w-128">
+                            <img src={newsfeed.feedImgURL} alt="Project Image" className="w-128 h-128 rounded-lg" />
+
+                            <div className="w-128 overflow-auto text-center">
+                                <p className="text-gray-900 font-bold">{newsfeed.title}</p>
+                                <p className="text-gray-700">{newsfeed.content}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="mb-4 flex space-x-2">
-                    <button className="px-4 py-2 bg-white text-blue-500 border-2 border-blue-500 rounded flex items-center space-x-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                        </svg>
-                        Like
-                    </button>
-                    <button className="px-4 py-2 bg-white text-blue-500 border-2 border-blue-500 rounded flex items-center space-x-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
-                        </svg>
-                        Comment
-                    </button>
+                    <div className="mb-4 flex space-x-2">
+                        <button className="px-4 py-2 bg-white text-blue-500 border-2 border-blue-500 rounded flex items-center space-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                            </svg>
+                            Like
+                        </button>
+                        <button className="px-4 py-2 bg-white text-blue-500 border-2 border-blue-500 rounded flex items-center space-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
+                            </svg>
+                            Comment
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     ))}
     </React.Fragment>
     ))}
