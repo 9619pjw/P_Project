3503492e2d76fd988c8834a6c6bc4ace0dfafd64
JokerBear_96 <9619pjw@gmail.com>
2023-12-11 12:13:33 +0900

    fix2 newsfeed create page

--- src/app/newsfeed/create/page.tsx
+++ src/app/newsfeed/create/page.tsx
@@ -27,8 +27,15 @@ type FeedInfo = {
     content: string; // 피드 내용
 };
 
+
 export default function NewsfeedCreatePage({ params }: { params: PageParams }){
     const [showUserInfo, setShowUserInfo] = useState(false);
+    const [feed, setFeed] = useState<FeedInfo>({
+        userId: 0,
+        image: null,
+        title: "",
+        content: ""
+    });
 
     let userJSON: UserJSON = {
         userId: 0,
@@ -41,87 +48,7 @@ export default function NewsfeedCreatePage({ params }: { params: PageParams }){
         followingCount : 0,
     };
     const [data, setData] = useState<UserJSON>(userJSON);
-    // TODO: GET - id에 맞는 사용자 정보 가져오기
     
-    const localStorage: Storage = window.localStorage;
-    const token = localStorage.getItem("accessToken");
-    let userId = params.id;
-
-    async function getUserInfo() {
-    // 로컬스토리지 토큰 가져오기
-        const localStorage: Storage = window.localStorage;
-        const token = localStorage.getItem("accessToken");
-        let userId = params.id;
-    // 요청 URL - PathVariable: userId
-        const getUserInfoURL: string = `https://funsns.shop:8000/user-service/user/${userId}`;
-
-        const response = await fetch(getUserInfoURL, {
-            method: "GET",
-            headers: {
-                "Credentials": "include",
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`,
-            },
-        })
-    .then((res) => res.json())
-    .then((data) => {
-        console.log("data:");
-        console.log(data);
-        if (data.code === "SUCCESS") {
-            console.log("사용자 정보를 불러오는데 성공했습니다.");
-        } else {
-            console.log("사용자 정보를 불러오는데 실패했습니다.");
-        }
-        return data;
-    })
-    .catch((error) => {
-        console.log(error);
-        throw new Error("서버 요청 실패!");
-    });
-
-    // 응답값 확인
-    let body: UserJSON;
-    if (response) {
-        // data부분만 가져오기
-        body = await response.data;
-        console.log("body:");
-        console.log(body);
-        return body;
-    } else {
-        console.log("응답이 없습니다.");
-        return null;
-    }
-    }
-
-    useEffect(() => {
-        async function fetchUserData() {
-        let userInfo = await getUserInfo();
-        if (userInfo) {
-            console.log("userInfo:");
-            console.log(userInfo);
-            setData(userInfo);
-            console.log("data:");
-            console.log(data);
-        }
-        }
-    fetchUserData();
-    setShowUserInfo(true);
-    }, []);
-
-
-    const [feed, setFeed] = useState<FeedInfo>({
-        userId: 0,
-        image: null,
-        title: "",
-        content: ""
-    });
-    useEffect(() => {
-        const localStorage: Storage = window.localStorage;
-        const userId = Number(localStorage.getItem("userId"));
-        
-        setFeed(prevState => ({ ...prevState, userId: userId }));
-    }, []);
-
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         setFeed({ ...feed, [e.target.name]: e.target.value });
     };
@@ -163,7 +90,7 @@ export default function NewsfeedCreatePage({ params }: { params: PageParams }){
 
             if (data.code === "SUCCESS") {
                 alert(data.message);
-                setFeed({ userId: userId, image: "", title: "", content: "" });
+                setFeed({ userId: feed.userId, image: "", title: "", content: "" });
             } else {
                 alert("피드 작성에 실패하였습니다.");
             }
@@ -171,6 +98,64 @@ export default function NewsfeedCreatePage({ params }: { params: PageParams }){
             console.error("Error:", error);
         }
     };
+
+    useEffect(() => {
+        const localStorage: Storage = window.localStorage;
+        const token = localStorage.getItem("accessToken");
+        let userId = params.id;
+
+        async function getUserInfo() {
+            const getUserInfoURL: string = `https://funsns.shop:8000/user-service/user/${userId}`;
+
+            const response = await fetch(getUserInfoURL, {
+                method: "GET",
+                headers: {
+                    "Credentials": "include",
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`,
+                },
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log("data:");
+                console.log(data);
+                if (data.code === "SUCCESS") {
+                    console.log("사용자 정보를 불러오는데 성공했습니다.");
+                } else {
+                    console.log("사용자 정보를 불러오는데 실패했습니다.");
+                }
+                return data;
+            })
+            .catch((error) => {
+                console.log(error);
+                throw new Error("서버 요청 실패!");
+            });
+
+            let body: UserJSON;
+            if (response) {
+                body = await response.data;
+                console.log("body:");
+                console.log(body);
+                return body;
+            } else {
+                console.log("응답이 없습니다.");
+                return null;
+            }
+        }
+
+        async function fetchUserData() {
+            let userInfo = await getUserInfo();
+            if (userInfo) {
+                setData(userInfo);
+            }
+        }
+
+        fetchUserData();
+        setShowUserInfo(true);
+
+        const userIdFromLS = Number(localStorage.getItem("userId"));
+        setFeed(prevState => ({ ...prevState, userId: userIdFromLS }));
+    }, []);
 
     return (
         <div className={styles.container}>
