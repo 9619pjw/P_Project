1d92bee382b76b0b3b241e67d104b7e880085e44
JokerBear_96 <9619pjw@gmail.com>
2023-12-13 00:34:37 +0900

    sdaf

--- src/app/newsfeed/detail/[id]/page.tsx
+++ src/app/newsfeed/detail/[id]/page.tsx
@@ -6,19 +6,7 @@ import React, { useEffect, useState } from 'react';
 import Link from "next/link";
 import { useRouter , useSearchParams } from 'next/navigation';
 
-type Newsfeed = {
-    feedId: number;
-    userId: number;
-    nickname: string;
-    title: string;
-    content: string;
-    profileImgURL: string;
-    feedImgURL: string;
-    likeCount: number;
-    commentCount: number;
-    isLiked: boolean;
-    createdDate: string;
-};
+
 
 type NewsfeedInfo = {
     feedId: number;
@@ -51,12 +39,12 @@ export default function FeedDetailPage(){
             const token = localStorage.getItem("accessToken");
             
             try{
-                const response = await fetch(`https://funsns.shop:8000/feed-service/feed/8`, 
+                const response = await fetch(`https://funsns.shop:8000/feed-service/feed/${feedId}`, 
                 {
                     method: "GET",
                     headers: {
-                        Credentials: "include",
-                        Authorization: `Bearer ${token}`,
+                        "Credentials": "include",
+                        "Authorization": `Bearer ${token}`,
                     },
                 }
             );
@@ -77,13 +65,7 @@ export default function FeedDetailPage(){
 
     return(
         <div>
-            <h1>{feedData.title}</h1>
-            <h2>작성자: {feedData.nickname}</h2>
-            <img src={feedData.profileImgURL} alt="Profile" />
-            <img src={feedData.feedImgURL} alt="Feed" />
-            <p>{feedData.content}</p>
-            <p>좋아요: {feedData.likeCount}</p>
-            <p>댓글: {feedData.commentCount}</p>
+        
         </div>
     );
 }
