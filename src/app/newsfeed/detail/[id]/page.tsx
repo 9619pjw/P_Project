aaf790ee5c2a9a2cd5bd56bbbb2501eecb14146a
JokerBear_96 <9619pjw@gmail.com>
2023-12-12 23:18:27 +0900

    fix

--- src/app/newsfeed/detail/[id]/page.tsx
+++ src/app/newsfeed/detail/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter , useSearchParams } from 'next/navigation';
 
 type Newsfeed = {
     feedId: number;
@@ -19,29 +19,37 @@ type Newsfeed = {
 
 export default function FeedDetailPage(){    
     const router = useRouter();
-    const { feedId } = router.query;
+    const params = useSearchParams();
+    const feedId  = params.get('gifticonId');
 
-    const localStorage: Storage = window.localStorage;
-    const token = localStorage.getItem("accessToken");
+   
     const [feedData, setFeedData] = useState<Newsfeed | null>(null);
     
     useEffect(() => {
         const fetchFeedDetail = async () => {
-            console.log(feedId); 
-            const response = await fetch(`https://funsns.shop:8000/feed-service/feed/${feedId}`, {
-            headers: {
-                "Credentials": "include",
-                "Authorization": `Bearer ${token}`,
-            },
-        });
-        const data = await response.json();
-        if (data.code === "SUCCESS") {
+            const localStorage: Storage = window.localStorage;
+            const token = localStorage.getItem("accessToken");
+            
+            try{
+                const response = await fetch(`https://funsns.shop:8000/feed-service/feed/${feedId}`, 
+                {
+                    method: "GET",
+                    headers: {
+                        Credentials: "include",
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            const data = await response.json();
             setFeedData(data.data);
-        }
+            } catch (error) {
+                console.error("Error:", error);
+            }
     };
-        
-        fetchFeedDetail();
-    }, [feedId,token]);
+        if(feedId){
+            fetchFeedDetail();
+        }
+    }, [feedId]);
     
     if (!feedData) {
         return <div>Loading...</div>;
