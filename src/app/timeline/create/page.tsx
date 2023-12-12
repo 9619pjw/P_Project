c376df9d442da244b759213d474b5ece4906ecac
JokerBear_96 <9619pjw@gmail.com>
2023-12-12 09:19:41 +0900

    add timeline page

---
+++ src/app/timeline/create/page.tsx
@@ -0,0 +1,207 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import Head from 'next/head';
+
+type UserJSON = {
+    userId: number;
+    name : string;
+    nickname: string;
+    introduction: string | null;
+    profileImage: string | null | undefined;
+    availableFollow : boolean;
+    followerCount : number;
+    followingCount : number;
+};
+
+type PageParams = {
+    id: number;
+};
+
+type FeedInfo = {
+    userId : number;
+    image: string | File | null;
+    title: string;
+    content: string;
+};
+
+export default function NewsfeedCreatePage({ params }: { params: PageParams }){
+    const router = useRouter();
+    const [showUserInfo, setShowUserInfo] = useState(false);
+    const [previewImage, setPreviewImage] = useState<string | null>(null);
+    const [feed, setFeed] = useState<FeedInfo>({
+        userId: 0,
+        image: null,
+        title: "",
+        content: ""
+    });
+
+    let userJSON: UserJSON = {
+        userId: 0,
+        name:".",
+        nickname: "default",
+        introduction: "",
+        profileImage: "/default-profile.png",
+        availableFollow : true,
+        followerCount : 0,
+        followingCount : 0,
+    };
+    const [data, setData] = useState<UserJSON>(userJSON);
+    
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setFeed({ ...feed, [e.target.name]: e.target.value });
+    };
+
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files.length > 0) {
+            const file = e.target.files[0];
+            setFeed({ ...feed, image: file });
+    
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setPreviewImage(reader.result as string);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (!feed.title || !feed.content) {
+            alert("모든 항목을 입력해주세요.");
+            return;
+        }
+
+        const localStorage: Storage = window.localStorage;
+        const token = localStorage.getItem("accessToken"); 
+
+        try {
+            const formData = new FormData();
+
+            let CreateFeedRequest = {
+                userId: feed.userId,
+                title: feed.title,
+                content: feed.content,
+            };
+
+            formData.append(
+                "content",
+                new Blob([JSON.stringify(CreateFeedRequest)], {
+                    type: "application/json",
+                })
+            );
+
+            if (feed.image) {
+                formData.append("image", feed.image);
+            }
+            // 데이터 확인
+            console.log(formData);
+            Array.from(formData.entries()).forEach(([key, value]) => {
+                console.log(key, value);
+            });
+
+            const response = await fetch("https://funsns.shop:8000/feed-service/feed", {
+                method: "POST",
+                headers: {
+                    "Credentials": "include",
+                    "Authorization": `Bearer ${token}`,
+                },
+                body: formData
+            });
+
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+
+            const data = await response.json();
+
+            if (data.code === "SUCCESS") {
+                alert(data.message);
+                setFeed({ userId: feed.userId, image: "", title: "", content: "" });
+                router.push('/newsfeed');
+            } else {
+                alert("피드 작성에 실패하였습니다.");
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+
+    useEffect(() => {
+        const localStorage: Storage = window.localStorage;
+        const token = localStorage.getItem("accessToken");
+        let userId = localStorage.getItem("userId");
+
+        async function getUserInfo() {
+            const getUserInfoURL: string = `https://funsns.shop:8000/user-service/user/${userId}`;
+
+            const response = await fetch(getUserInfoURL, {
+                method: "GET",
+                headers: {
+                    "Credentials": "include",
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`,
+                },
+            });
+
+            if(response.ok) {
+                const responseBody = await response.json();
+                console.log("data:");
+                console.log(responseBody);
+                if (responseBody.code === "SUCCESS") {
+                    console.log("사용자 정보를 불러오는데 성공했습니다.");
+                    return responseBody.data;
+                } else {
+                    console.log("사용자 정보를 불러오는데 실패했습니다.");
+                }
+            } else {
+                console.log("서버 요청 실패!");
+                throw new Error("서버 요청 실패!");
+            }
+        }
+
+        async function fetchUserData() {
+            let userInfo = await getUserInfo();
+            if (userInfo) {
+                setData(userInfo);
+            }
+        }
+
+        fetchUserData();
+        setShowUserInfo(true);
+
+        const userIdFromLS = Number(localStorage.getItem("userId"));
+        setFeed(prevState => ({ ...prevState, userId: userIdFromLS }));
+    }, []);
+
+    return (
+        <>
+            <Head>
+                <title>Create NewsFeed</title>
+            </Head>
+        <div className="bg-gray-50 container mx-auto p-6">
+        <form onSubmit={handleSubmit} className="space-y-4">
+            <input type="hidden" name="userId" value={feed.userId} />
+            <label className="block">
+                <span className="text-gray-700">제목:</span>
+                <input type="text" name="title" value={feed.title} onChange={handleChange} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm" />
+            </label>
+            <label className="block">
+                <span className="text-gray-700">내용:</span>
+                <textarea name="content" value={feed.content} onChange={handleChange} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"/>
+            </label>
+            <label className="block">
+                <span className="text-gray-700">이미지:</span>
+                <input type="file" name="image" onChange={handleImageChange} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"/>
+            </label>
+            {previewImage && <img src={previewImage} alt="Preview" className="mt-4"/>}
+            {/* {feed.image && <img src={feed.image} alt="Preview" />} */}
+            <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded mb-8">피드 작성</button>
+        </form>
+    </div>
+    </>
+    );
+}
--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -1,70 +1,131 @@
-import { useInView } from "react-intersection-observer";
-// import { useInfiniteQuery } from '@tanstack/react-query'
+"use client";
 
-import React from 'react';
+import { QueryClient, QueryClientProvider, useInfiniteQuery, QueryFunctionContext } from 'react-query';
+import { useInView } from 'react-intersection-observer';
+import React, { useEffect, useState } from 'react';
+import Link from "next/link";
 
-export default function TimelinePage(){
+type Newsfeed = {
+    feedId: number;
+    userId: number;
+    nickname: string;
+    title: string;
+    content: string;
+    profileImgURL: string;
+    feedImgURL: string;
+    likeCount: number;
+    commentCount: number;
+    isLiked: boolean;
+    createdDate: string;
+};
 
-    const fetchMorePosts = () => {
-        // Mock fetch function to simulate loading new content ...
+type NewsfeedComponentProps = {
+    loadData: boolean;
+    fetchNewsfeeds: (context: QueryFunctionContext) => Promise<{ newsfeeds: Newsfeed[]; nextCursor: number }>;
+    token: string | null;
+};
+
+export default function NewsfeedPage(){
+    const [token, setToken] = useState<string | null>(null);
+    const [loadData, setLoadData] = useState(false);
+
+    useEffect(() => {
+        setToken(localStorage.getItem("accessToken"));
+        setLoadData(true); 
+    }, []);
+
+    const fetchNewsfeeds = async ({ pageParam = 0 }: QueryFunctionContext) => {
+        const response = await fetch(`https://funsns.shop:8000/feed-service/feed/newsfeed?cursor=${pageParam}&size=5`, {
+        headers: {
+            "Credentials": "include",
+            "Authorization": `Bearer ${token}`,
+        },
+    });
+    const data = await response.json();
+    
+    return { newsfeeds: data.data, nextCursor: data.data[data.data.length - 1].feedId };
     };
-    // React.useEffect(() => {
-    //     let observer;
-    //     const list = document.querySelector('#post-list');
-    //     const lastItem = () => list.lastElementChild;
-    
-    //     const createObserver = () => {
-    //       let options = {
-    //         root: null,
-    //         rootMargin: "20px",
-    //         threshold: 1.0
-    //       };
-    
-    //       observer = new IntersectionObserver((entries, observer) => {
-    //         entries.forEach(entry => {
-    //           if (entry.isIntersecting) {
-    //             fetchMorePosts();
-    //           }
-    //         });
-    //       }, options);
-    
-    //       observer.observe(lastItem());
-    //     };
-    
-    //     createObserver();
-    //   }, []);
-    
 
-
+    const queryClient = new QueryClient();
     return (
-        <div className="bg-gray-50 flex justify-center p-6">
-        <div className="bg-white shadow-md rounded-lg overflow-hidden">
-            <div className="p-6">
-                <div className="flex items-center mb-4">
-                    <div className="mr-2">
-                        <img src="https://unsplash.it/32/32" alt="User Avatar" className="rounded-full" />
+        <QueryClientProvider client={queryClient}>
+            <NewsfeedComponent loadData={loadData} fetchNewsfeeds={fetchNewsfeeds} token={token}/>
+        </QueryClientProvider>
+    );
+}
+
+function NewsfeedComponent({loadData, fetchNewsfeeds, token} : NewsfeedComponentProps) {
+
+    const {
+        data,
+        fetchNextPage,
+        hasNextPage,
+        isFetching,
+    } = useInfiniteQuery('newsfeeds', fetchNewsfeeds, {
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+        enabled: !!token && loadData, 
+    });
+
+    const { ref, inView } = useInView({
+        threshold: 1,
+    });
+
+    useEffect(() => {
+        if (inView && hasNextPage && !isFetching) {
+            fetchNextPage();
+        }
+    }, [inView, hasNextPage, isFetching, fetchNextPage]);
+    
+    return (
+        <div>
+            <Link href="/newsfeed/create">
+                <button className="px-4 py-2 bg-blue-500 text-white rounded mb-8">
+                    피드 생성
+                </button>
+            </Link>
+            {data?.pages.flatMap((group, i) => (
+            <React.Fragment key={i}>
+                {group.newsfeeds.map((newsfeed : Newsfeed) => (
+                <div key={newsfeed.feedId} className="bg-gray-50 flex justify-center p-6">
+                    <div className="bg-white shadow-md rounded-lg w-full max-w-lg">
+                        <div className="p-6">
+                            <div className="flex items-center mb-4">
+                                <div className="mr-2">
+                                    <img src={newsfeed.profileImgURL} alt="User Avatar" className="rounded-full w-12 h-12" />
+                                </div>
+                            <div>
+                            <p className="text-sm font-semibold">{newsfeed.nickname}</p>
+                            <p className="text-xs text-gray-500">게시일 : {newsfeed.createdDate}</p>
+                        </div>
                     </div>
-                    <div>
-                        <p className="text-sm font-semibold">작성자 : </p>
-                        <p className="text-xs text-gray-500">작성일 : 2023/12/03</p>
+                    <div className="mb-4">
+                        <img src={newsfeed.feedImgURL} alt="Project Image" className="w-full h-128 rounded-lg object-cover" />
                     </div>
-                </div>
-                <div className="mb-4">
-                    <img src="https://unsplash.it/600/300" alt="Project Image" className="w-auto h-auto rounded-lg" />
-                </div>
-                <div className="mb-4">
-                    <p className="text-gray-700">콘텐츠 들어갈 부분</p>
-                </div>
-                <div className="mb-4">
-                    <div>
-                        <button className="px-4 py-2 bg-blue-500 text-white rounded ">Like</button>
-                        <br />
-                        <br/ > 
-                        <button className="px-4 py-2 bg-blue-500 text-white rounded">Comment</button>
+                    <div className="mb-4 text-center">
+                            <p className="text-gray-900 font-bold">{newsfeed.title}</p>
+                            <p className="text-gray-700">{newsfeed.content}</p>
+                    </div>
+                    <div className="mb-4 flex space-x-2">
+                        <button className="px-4 py-2 bg-white text-blue-500 border-2 border-blue-500 rounded flex items-center space-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                            </svg>
+                            Like
+                        </button>
+                        <button className="px-4 py-2 bg-white text-blue-500 border-2 border-blue-500 rounded flex items-center space-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
+                            </svg>
+                            Comment
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    );
+    ))}
+    </React.Fragment>
+    ))}
+    <div ref={ref} />
+</div>
+);
 }
