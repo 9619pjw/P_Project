03db94e05464323b0bc7a684f2da85ed66869d2f
JokerBear_96 <9619pjw@gmail.com>
2023-12-14 15:44:00 +0900

    fix comment button

--- src/app/newsfeed/detail/[id]/page.tsx
+++ src/app/newsfeed/detail/[id]/page.tsx
@@ -431,19 +431,19 @@ export default function FeedDetailPage(props: ReadProps) {
                                 </p>
                                 <p className="text-gray-700"  dangerouslySetInnerHTML={{ __html: comment.content.replace(/\n/g, '<br />') }}></p>
                             </div>
-                            {/* <button onClick={() => likeComment(comment.commentId)} className="px-4 py-2 bg-white text-blue-500 border-2 border-blue-500 rounded flex items-center space-x-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                                </svg>
-                                좋아요 : {comment.likeCount}
-                            </button> */}
                                 {comment.isLiked ? (
-                                    <button onClick={() => unlikeComment(comment.commentId)} className="px-4 py-2 bg-gray-500 text-white rounded">
-                                        좋아요 취소
+                                    <button onClick={() => unlikeComment(comment.commentId)} className="px-4 py-2 bg-white text-blue-500 border-2 border-blue-500 rounded flex items-center space-x-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                        </svg>
+                                        좋아요 취소 : {comment.likeCount}
                                     </button>
                                 ) : (
-                                    <button onClick={() => likeComment(comment.commentId)} className="px-4 py-2 bg-blue-500 text-white rounded">
-                                        좋아요
+                                    <button onClick={() => likeComment(comment.commentId)} className="px-4 py-2 bg-white text-blue-500 border-2 border-blue-500 rounded flex items-center space-x-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                        </svg>
+                                        좋아요 : {comment.likeCount}
                                     </button>
                                 )}
                                 {comments.map((comment: CommentInfo) => (
