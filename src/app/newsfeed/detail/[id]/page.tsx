09efb689a74b1d1a31741cdef6cd1e66edd70031
JokerBear_96 <9619pjw@gmail.com>
2023-12-14 19:41:36 +0900

    fix feed like unlike

--- src/app/newsfeed/detail/[id]/page.tsx
+++ src/app/newsfeed/detail/[id]/page.tsx
@@ -401,7 +401,7 @@ export default function FeedDetailPage(props: ReadProps) {
                         <p className="text-gray-700" dangerouslySetInnerHTML={{ __html: feedData.content.replace(/\n/g, '<br />') }}></p>
                     </div>
                 <div className="mb-4 flex space-x-2 py-4 border-b">
-                {isLiked ? 
+                {feedData.isLiked ? 
                     <button onClick={handleUnlike} className="px-4 py-2 bg-white text-blue-500 border-2 border-blue-500 rounded flex items-center space-x-2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
