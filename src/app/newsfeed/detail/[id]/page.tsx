d72e7ba0d04f45ffb9c12ae884333c2bcd86c98d
JokerBear_96 <9619pjw@gmail.com>
2023-12-15 20:14:01 +0900

    add like phrase

--- src/app/newsfeed/detail/[id]/page.tsx
+++ src/app/newsfeed/detail/[id]/page.tsx
@@ -401,22 +401,27 @@ export default function FeedDetailPage(props: ReadProps) {
                         <p className="text-gray-900 font-bold">{feedData.title}</p>
                         <p className="text-gray-700" dangerouslySetInnerHTML={{ __html: feedData.content.replace(/\n/g, '<br />') }}></p>
                     </div>
-                <div className="mb-4 flex space-x-2 py-4 border-b">
-                {feedData.isLiked ? (
-                    <button onClick={handleUnlike} className="px-4 py-2 bg-white text-blue-500 border-2 border-blue-500 rounded flex items-center space-x-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                        </svg>
-                        좋아요 취소 : {feedData.likeCount}
-                    </button> 
-                ):(
-                    <button onClick={handleLike} className="px-4 py-2 bg-white text-blue-500 border-2 border-blue-500 rounded flex items-center space-x-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                        </svg>
-                        좋아요 : {feedData.likeCount}
-                    </button>
-                )}
+                <div className="mb-4 flex flex-col space-y-4 py-4 border-b">
+                    <Link href={`/newsfeed/detail/${feedData.feedId}/likefeed`}>
+                        <a className="text-black-500">{feedData.likeCount} 명이 좋아합니다</a>
+                    </Link>
+                    <div className="flex space-x-2">
+                        {feedData.isLiked ? (
+                            <button onClick={handleUnlike} className="px-4 py-2 bg-white text-blue-500 border-2 border-blue-500 rounded flex items-center space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                </svg>
+                                좋아요 취소
+                            </button> 
+                        ):(
+                            <button onClick={handleLike} className="px-4 py-2 bg-white text-blue-500 border-2 border-blue-500 rounded flex items-center space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                </svg>
+                                좋아요 : {feedData.likeCount}
+                            </button>
+                        )}
+                    </div>
                     <button className="px-4 py-2 bg-white text-blue-500 border-2 border-blue-500 rounded flex items-center space-x-2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
