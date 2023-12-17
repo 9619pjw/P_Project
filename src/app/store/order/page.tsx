33468f4abb967920851e818c94467a903e38e020
JokerBear_96 <9619pjw@gmail.com>
2023-12-17 14:44:24 +0900

    edit order page tailwind css

--- src/app/store/order/page.tsx
+++ src/app/store/order/page.tsx
@@ -10,30 +10,29 @@ import Image from "next/image";
 
 // 주문 정보
 type OrderInfo = {
-    gifticonId?: number; // 기프티콘 ID
-    toUserNickName: string; // 기프티콘을 받는 사용자의 닉네임
-    amount: number; // 기프티콘 주문 수량
-    letter: string; // 기프티콘에 담을 편지
+  gifticonId?: number; // 기프티콘 ID
+  toUserNickName: string; // 기프티콘을 받는 사용자의 닉네임
+  amount: number; // 기프티콘 주문 수량
+  letter: string; // 기프티콘에 담을 편지
 };
 
 // 주문자 정보
 type UserInfo = {
-    userId: number;
-    nickname: string;
-    balance: number;
+  userId: number;
+  nickname: string;
+  balance: number;
 };
 
 // 기프티콘 정보
 type GiftInfo = {
-    gifticonId ?: number; // 기프티콘 id
-    image: string; // 기프티콘 이미지 
-    categoryName: string; // 카테고리 이름
-    gifticonName: string; // 상품명
-    description: string; // 설명
-    price: number; // 가격
-    amount: number; // 수량
-  };
-  
+  gifticonId?: number; // 기프티콘 id
+  image: string; // 기프티콘 이미지
+  categoryName: string; // 카테고리 이름
+  gifticonName: string; // 상품명
+  description: string; // 설명
+  price: number; // 가격
+  amount: number; // 수량
+};
 
 export default function GiftOrderPage() {
   const [orderInfo, setOrderInfo] = useState<OrderInfo>({
@@ -46,8 +45,7 @@ export default function GiftOrderPage() {
   const [gifticon, setGifticon] = useState<GiftInfo | null>(null);
   const router = useRouter();
   const params = useSearchParams();
-  const gifticonId = params.get('gifticonId'); // 쿼리 파라미터 'gifticonId' 받아
-
+  const gifticonId = params.get("gifticonId"); // 쿼리 파라미터 'gifticonId' 받아
 
   // 기프티콘 정보 불러옴
   useEffect(() => {
@@ -56,16 +54,17 @@ export default function GiftOrderPage() {
       const token = localStorage.getItem("accessToken");
 
       try {
-        const response = await fetch(`https://funsns.shop:8000/gifticon-service/gifticon/${gifticonId}`,
-        {
-          method:"GET",
-          headers: {
-            Credentials: "include",
-            ContentType: "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+        const response = await fetch(
+          `https://funsns.shop:8000/gifticon-service/gifticon/${gifticonId}`,
+          {
+            method: "GET",
+            headers: {
+              Credentials: "include",
+              ContentType: "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
         const data = await response.json();
         // 기프티콘 정보 저장
         setGifticon(data.data);
@@ -81,11 +80,13 @@ export default function GiftOrderPage() {
     }
   }, [gifticonId]);
 
- // 뒤로가기 버튼
+  // 뒤로가기 버튼
   const handleGoBack = () => {
     window.location.href = "/store";
   };
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     setOrderInfo({ ...orderInfo, [e.target.name]: e.target.value });
   };
 
@@ -111,11 +112,11 @@ export default function GiftOrderPage() {
             "Content-type": "application/json",
             Authorization: `Bearer ${token}`,
           },
-          body : JSON.stringify ({
-            gifticonId : orderInfo.gifticonId,
-            toUserNickname : orderInfo.toUserNickName,
-            amount : orderInfo.amount,
-            letter : orderInfo.letter,
+          body: JSON.stringify({
+            gifticonId: orderInfo.gifticonId,
+            toUserNickname: orderInfo.toUserNickName,
+            amount: orderInfo.amount,
+            letter: orderInfo.letter,
           }),
         }
       );
@@ -139,84 +140,86 @@ export default function GiftOrderPage() {
   };
 
   return (
-    <div>
-        <br />
-        <br />
-        <form onSubmit={handleSubmit}  className={styles["modal-form"]}
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                backgroundColor: "white",
-                borderRadius: "10px",
-                padding: "20px",
-                width: "300px",
-              }}>
-        
+    <div className="bg-gray-100 container mx-auto px-4 py-8 flex-1 flex flex-col items-center justify-start pt-5">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center justify-center bg-white rounded-lg p-5 w-72"
+      >
         {gifticon && (
           <Image
             src={gifticon.image} // 기프티콘의 이미지 URL을 사용
             alt={`기프티콘 ${gifticonId}`}
             width={500}
             height={500}
-          /> 
+          />
         )}
-        <div>
-        <p>상품명 : {gifticon?.gifticonName}</p>
-        <br/>
-        기프티콘 ID: 
-        <input
-          type="number"
-          name="gifticonId"
-          value={orderInfo.gifticonId}
-          onChange={handleChange}
-          placeholder="기프티콘 ID"
-          required
-          readOnly
-        />   
-        <br />
-        <br />
-        받을사람 닉네임 :
-        <input
-          type="text"
-          name="toUserNickName"
-          value={orderInfo.toUserNickName}
-          onChange={handleChange}
-          placeholder="기프티콘 받을 사용자의 닉네임"
-          required
-        />
-        <br />
-        <br />
-        주문 수량 :
-        <input
-          type="number"
-          name="amount"
-          value={orderInfo.amount}
-          onChange={handleChange}
-          placeholder="기프티콘 주문 수량"
-          required
-        />
-        <br />
-        <br />
-        편지 :
-        <textarea
-          name="letter"
-          value={orderInfo.letter}
-          onChange={handleChange}
-          placeholder="기프티콘에 담을 편지"
-          required
-          style={{width: '100%', height: '200px', fontSize: '16px', padding: '5px', borderRadius:'10px'}} 
-        />
+        <div className="mt-4">
+          <p>상품명 : {gifticon?.gifticonName}</p>
+          <div className="mt-4">
+            <label>기프티콘 ID:</label>
+            <input
+              type="number"
+              name="gifticonId"
+              value={orderInfo.gifticonId}
+              onChange={handleChange}
+              placeholder="기프티콘 ID"
+              required
+              readOnly
+              className="mt-2 border border-gray-300 px-2 w-full"
+            />
+          </div>
+          <div className="mt-4">
+            <label>받을사람 닉네임:</label>
+            <input
+              type="text"
+              name="toUserNickName"
+              value={orderInfo.toUserNickName}
+              onChange={handleChange}
+              placeholder="기프티콘 받을 사용자의 닉네임"
+              required
+              className="mt-2 border border-gray-300 px-2 w-full"
+            />
+          </div>
+          <div className="mt-4">
+            <label>주문 수량:</label>
+            <input
+              type="number"
+              name="amount"
+              value={orderInfo.amount}
+              onChange={handleChange}
+              placeholder="기프티콘 주문 수량"
+              required
+              className="mt-2 border border-gray-300 px-2 w-full"
+            />
+          </div>
+          <div className="mt-4">
+            <label>편지:</label>
+            <textarea
+              name="letter"
+              value={orderInfo.letter}
+              onChange={handleChange}
+              placeholder="기프티콘에 담을 편지"
+              required
+              className="mt-2 border border-gray-300 px-2 w-full h-48"
+            />
+          </div>
         </div>
-        <br />
-        <br />
-        <button type="submit" className={styles.addButton}>
+        <div className="flex justify-between mt-4">
+          <button
+            type="submit"
+            className="px-4 py-2 bg-blue-500 text-white rounded mr-2"
+          >
             선물하기
-        </button>
-        <button type="button" onClick={handleGoBack} className={styles.backbutton}> 뒤로가기
-        </button>
-        </form>
+          </button>
+          <button
+            type="button"
+            onClick={handleGoBack}
+            className="px-4 py-2 bg-red-500 text-white rounded"
+          >
+            뒤로가기
+          </button>
+        </div>
+      </form>
     </div>
-    );
+  );
 }
